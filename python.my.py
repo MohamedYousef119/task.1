950e3f18a511332e124e9a0a7ab1d89abe50aa19
MohamedYousef119 <mohamedyossefibrahim23@gmail.com>
2022-08-13 17:58:12 +0200

    python.my.py

---
+++ python.my.py
@@ -0,0 +1,58 @@
+1- x = true
+2- y = int()
+3- z = float()
+4- s = 'my name is mohamed'
+5- float2 =float(int(y))
+6- #yes we can
+7-  L = [1,2,3,4,5]
+8-  t = (10,11,12,13,14,15)
+9-  type2 =type(L)
+10- d = {'mohamed':80,'ail':90,'ahmed':70}
+11- #yes we can
+12- #interpreted
+13- #high =It is easy to understand ,,low =It is tough to understand
+14- #= equal ,, == is equal 
+15- #not eqaul
+16- #Arithmetic operators ,Assignment operators ,Comparison operators
+17- x = 10
+18- x*=15
+19- x/=5
+20- x*=10
+21- x%=3
+22- print(11%4)
+23- print(2**3)
+24- x = 11//3
+25- #yes we can by uesing (//)
+26- x =10 ,x>15
+27- x=10
+    if x>15:
+        print(true)
+    else print (x is smaller than 15)
+28- #to check if the conditions is true 
+29- #and = check one by one ,, all = check list of condition 
+30- #or = check one by one,,, any = check list of condition
+31- #and,,,all
+32- #if= use once ,,, elif= use more than one
+33- #yes we can use more than one
+34- #no we can not ues more than one
+34- x = 300
+print ("x<320") if x < 320 else print(False)
+35- #no if the condition is true
+36- # default
+37- l= [2,4,6,8,10]
+if 4 in l:
+     print ("yes")
+else:
+    print ("no")
+if 4 and 6 in l:
+    print ("yes")
+else:
+    print ('no')
+if 3 or 6 in l:
+    print('yes')
+else:
+    print ('no')
+if 2,3 and 5 in l:
+    print ('yes')
+else:
+    print ("no")
